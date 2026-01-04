34ed6700a839405d0f3f774e2298f14fe51f8aef
realslimtaek <xorgud777@gmail.com>
2026-01-04 17:19:59 +0900

    feat: Jpa 및 Mysql 데이터베이스 연결

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("jvm") version "2.1.0"
+	kotlin("plugin.spring") version "2.1.0"
+    kotlin("plugin.jpa") version "2.1.0"
 	id("org.springframework.boot") version "4.0.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -20,8 +21,12 @@ repositories {
 }
 
 dependencies {
+    runtimeOnly("com.mysql:mysql-connector-j")
 	implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 //	testImplementation("org.springframework.boot:spring-boot-starter-test")
 //	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 //	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -32,7 +37,7 @@ kotlin {
 		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
 	}
 }
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+//
+//tasks.withType<Test> {
+//	useJUnitPlatform()
+//}
